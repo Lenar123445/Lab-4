57d26e2d4bee1c560aea0eb0a9fa057248e60934
saifu <saifu@DESKTOP-1G39U7C>
2023-03-22 17:33:22 +0300

    Добавьте файлы проекта.

---
+++ massive/Class1.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dynamic_massive
+{
+    internal class DynamicArray
+    {
+        public int[] array;
+        public int size;
+        public int capacity;
+
+        public DynamicArray(int initialcapacity = 4)
+        {
+            array = new int[initialcapacity];
+            size = 0;
+            capacity = initialcapacity;
+        }
+
+        public int this[int index]
+        {
+            get { return array[index]; }
+            set { array[index] = value; }
+        }
+
+        public void Add_End(int value)
+        {
+            if (size == capacity)
+            {
+                Resize(capacity * 2);
+            }
+            array[size] = value;
+            size++;
+        }
+        public void Delete_End()
+        {
+            size--;
+        }
+
+        public void Insert(int index, int value)
+        {
+            if (size == capacity)
+            {
+                Resize(capacity * 2);
+            }
+            for (int i = size; i > index; i--)
+            {
+                array[i] = array[i - 1];
+            }
+            array[index] = value;
+            size++;
+        }
+
+        public void Remove(int index)
+        {
+            for (int i = index; i < size - 1; i++)
+            {
+                array[i] = array[i + 1];
+            }
+            size--;
+
+            if (size < capacity / 2)
+            {
+                Resize(capacity / 2);
+            }
+        }
+
+        public void Remove_Value(int value)
+        {
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i] == value)
+                {
+                    Remove(i);
+                }
+            }
+        }
+        public int Max()
+        {
+            int maxindex = 0;
+            for (int i = 0; i < size; i++)
+            {
+                if (array[i] > array[maxindex])
+                {
+                    maxindex = i;
+                }
+            }
+            return maxindex;
+        }
+        public bool Full()
+        {
+            return size == capacity;
+        }
+
+        public int Count()
+        {
+            return size;
+        }
+
+        public void Resize(int NewCapacity)
+        {
+            int[] newArray = new int[NewCapacity];
+            for (int i = 0; i < size; i++)
+            {
+                newArray[i] = array[i];
+            }
+            array = newArray;
+            capacity = NewCapacity;
+        }
+
+        public void Print()
+        {
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ massive/Program.cs
@@ -0,0 +1,69 @@
+﻿namespace dynamic_massive
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] matrix = new int[5, 6];
+            Random rand = new Random();
+
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    matrix[i, j] = rand.Next(-100, 101);
+                    Console.Write("{0}\t", matrix[i, j]);
+                }
+                Console.WriteLine();
+            }
+            DynamicArray positive = new DynamicArray();
+            DynamicArray negative = new DynamicArray();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    if (matrix[i, j] > 0)
+                    {
+                        positive.Add_End(matrix[i, j]);
+                    }
+                    else
+                    {
+                        negative.Add_End(matrix[i, j]);
+                    }
+                }
+            }
+            Console.WriteLine("\nМассив с положительными элементами:");
+            for (int i = 0; i < positive.size; i++)
+            {
+                Console.Write(positive[i] + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("\nМассив с отрицательными элементами: ");
+            for (int i = 0; i < negative.size; i++)
+            {
+                Console.Write(negative[i] + " ");
+            }
+            Console.WriteLine("\nМассив без последнего элемента: ");
+            negative.Delete_End();
+            negative.Print();
+            Console.WriteLine("\nМассив без пятого элемента: ");
+            negative.Remove(4);
+            negative.Print();
+            Console.WriteLine("\nМассив с 10 в конце: ");
+            negative.Add_End(10);
+            negative.Print();
+            Console.WriteLine("\nМассив с 10 на 3 месте: ");
+            negative.Insert(2, 10);
+            negative.Print();
+            Console.WriteLine("\nМассив без 10: ");
+            negative.Remove_Value(10);
+            negative.Print();
+            Console.WriteLine("Индекс максимального элемент массива: " + negative.Max());
+            Console.WriteLine("Заполнен ли массив: " + negative.Full());
+            Console.WriteLine("Количество элементов в массиве: " + negative.Count());
+
+            
+            
+        }
+    }
+}
